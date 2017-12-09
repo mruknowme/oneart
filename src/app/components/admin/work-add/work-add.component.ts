c3f83e3daf57dfdfff0342c110d9bc5fb7dad236
Max Valetov <valetovmax@gmail.com>
2017-12-09 23:47:43 +1100

    implementation of adding new work

--- src/app/components/admin/work-add/work-add.component.ts
+++ src/app/components/admin/work-add/work-add.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
@@ -35,22 +35,40 @@ export interface Work {
 })
 export class WorkAddComponent implements OnInit {
 
+  test: string;
+
   genresColRef: AngularFirestoreCollection<Genres>;
   genres$: Observable<Genres[]>;
 
   creatorsColRef: AngularFirestoreCollection<Creators>;
   creators$: Observable<Creators[]>;
 
-  private worksColRef: AngularFirestoreCollection<Creators>;
+  creatorsOptions: any;
+  genreOptions: any;
+
+  private worksColRef: AngularFirestoreCollection<Work>;
 
   public worksAddForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private afs: AngularFirestore, @Inject(MAT_DIALOG_DATA) public dialogData: any) {
+  public creator: Creators = {
+    about: '',
+    name: '',
+    username: ''
+  };
+  public genre: Genres = {
+    alias: '',
+    description: '',
+    title: ''
+  };
+
+  constructor(private fb: FormBuilder, private afs: AngularFirestore) {
     this.genresColRef = this.afs.collection<Genres>('genres', ref => ref.orderBy('title'));
     this.genres$ = this.genresColRef.valueChanges();
+    this.genres$.subscribe(data => this.genreOptions = data);
 
     this.creatorsColRef = this.afs.collection<Creators>('creators', ref => ref.orderBy('name'));
     this.creators$ = this.creatorsColRef.valueChanges();
+    this.creators$.subscribe(data => this.creatorsOptions = data);
 
     this.createWorksAddForm();
 
@@ -59,6 +77,10 @@ export class WorkAddComponent implements OnInit {
 
   createWorksAddForm() {
     this.worksAddForm = this.fb.group({
+      creator: ['', [Validators.required]],
+      creator_username: ['', [Validators.required]],
+      genre: ['', [Validators.required]],
+      genre_alias: ['', [Validators.required]],
       title: [ '', [Validators.required, Validators.minLength(3), Validators.maxLength(70)] ],
       description: [ '', [Validators.required, Validators.minLength(50), Validators.maxLength(5000)] ],
       price: [ '', [Validators.required, Validators.min(0)] ],
@@ -67,6 +89,7 @@ export class WorkAddComponent implements OnInit {
   }
 
   onSubmit(button, text) {
+    console.log(this.worksAddForm);
     if (this.worksAddForm.valid) {
       button.textContent = text;
       button.disabled = true;
@@ -79,17 +102,34 @@ export class WorkAddComponent implements OnInit {
         genre_alias: this.worksAddForm.controls.genre_alias.value.trim(),
         images: [''],
         link: this.worksAddForm.controls.link.value.trim(),
-        price: this.worksAddForm.controls.price.value.trim(),
+        price: this.worksAddForm.controls.price.value,
         title: this.worksAddForm.controls.title.value.trim()
       };
-      // t
-      console.log(newWork);
+      this.worksColRef.add({
+        created_at: newWork.created_at,
+        creator: newWork.creator,
+        creator_username: newWork.creator_username,
+        description: newWork.description,
+        genre: newWork.genre,
+        genre_alias: newWork.genre_alias,
+        images: newWork.images,
+        link: newWork.link,
+        price: newWork.price,
+        title: newWork.title
+      });
     } else {
       alert('Возникла ошибка! Перезагрузите страницу и попробуйте заново.');
     }
   }
 
-  ngOnInit() {
+  changeGenre() {
+    this.genre = this.genreOptions.find(item => item.title === this.worksAddForm.controls.genre.value);
   }
+
+  changeCreator() {
+    this.creator = this.creatorsOptions.find(item => item.name === this.worksAddForm.controls.creator.value);
+  }
+
+  ngOnInit() {}
 
 }
