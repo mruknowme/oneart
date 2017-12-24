c69bfd5b74cc777a739e35f2077a9a17c1d166b6
Max Valetov <valetovmax@gmail.com>
2017-12-24 22:21:46 +1100

    implemented hack to trigger :hover on tap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -35,7 +35,7 @@ export class HomeComponent implements OnInit {
   }
 
   onTap() {
-    alert('works');
+    console.log('tapped');
   }
 
   ngOnInit() {
